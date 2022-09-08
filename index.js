62402e0becaa0d7f068ca4714d543e6dd47d2e1b
onesmus kamara <onesmusqamara@gmail.com>
2022-09-08 15:30:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+ 
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +14,9 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books/5')
+  .then(resp => resp.json())
+  .then (json => renderBooks(json));
+}
+}
